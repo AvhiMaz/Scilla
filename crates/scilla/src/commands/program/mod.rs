31582a2e70019f2f4665a3d0277cc1d9b45a372b
AvhiMaz <avhimazumder5@outlook.com>
2026-01-23 14:25:05 +0530

    refactor: optimize program extend by fetching account data once
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- crates/scilla/src/commands/program/mod.rs
+++ crates/scilla/src/commands/program/mod.rs
@@ -61,15 +61,15 @@ impl fmt::Display for ProgramShared {
     }
 }
 
-impl ProgramShared {
-    async fn process_command(&self, ctx: &ScillaContext) -> CommandFlow {
+impl Command for ProgramShared {
+    async fn process_command(&self, ctx: &mut ScillaContext) -> anyhow::Result<CommandFlow> {
         match self {
             ProgramShared::Deploy => todo!(),
             ProgramShared::Upgrade => todo!(),
             ProgramShared::Build => build::process_build().await,
             ProgramShared::Close => todo!(),
-            ProgramShared::Extend => extend::process_extend(ctx).await,
-            ProgramShared::GoBack => CommandFlow::NavigateTo(prompt_go_back()),
+            ProgramShared::Extend => Ok(extend::process_extend(ctx).await),
+            ProgramShared::GoBack => Ok(CommandFlow::NavigateTo(prompt_go_back())),
         }
     }
 }
@@ -100,7 +100,7 @@ impl Command for ProgramCommand {
     async fn process_command(&self, ctx: &mut ScillaContext) -> anyhow::Result<CommandFlow> {
         ctx.get_nav_context_mut()
             .checked_push(NavigationSection::Program);
-        let res = match self {
+        match self {
             ProgramCommand::ProgramLegacy => {
                 ctx.get_nav_context_mut()
                     .checked_push(NavigationSection::ProgramLegacy);
@@ -114,9 +114,8 @@ impl Command for ProgramCommand {
                 command.process_command(ctx).await
             }
             ProgramCommand::GoBack => {
-                return Ok(CommandFlow::NavigateTo(NavigationTarget::PreviousSection));
+                Ok(CommandFlow::NavigateTo(NavigationTarget::PreviousSection))
             }
-        };
-        Ok(res)
+        }
     }
 }
